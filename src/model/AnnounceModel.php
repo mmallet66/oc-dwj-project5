2249bbec2a2c77feead44e91993efba561e8271c
mmallet66 <m.mallet@outlook.fr>
2020-04-16 17:22:18 +0200

    creation of announce model

---
+++ src/model/AnnounceModel.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Occazou\Src\Model;
+
+require_once 'Model.php';
+
+/**
+ * Class AnnounceModel
+ */
+class AnnounceModel extends Model
+{
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->dbConnect();
+    }
+
+    /**
+     * Register a new announce in the database
+     * 
+     * @param object $announce
+     * 
+     * @return int or false
+     */
+    public function addAnnounce(object $announce)
+    {
+        $req = $this->db->prepare('INSERT INTO announces(author_id, title, text, price, picture_id) VALUES(:authorId, :title, :text, :price, :pictureId)');
+
+        return $req->execute(array(
+            ':authorId'=>$announce->getAuthorId(),
+            ':title'=>$announce->getTitle(),
+            ':text'=>$announce->getText(),
+            ':price'=>$announce->getPrice(),
+            ':pictureId'=>$announce->getPictureId()
+        ));
+    }
+
+    /**
+     * Retrieves an announce's datas from the database, and returns an array of datas
+     * 
+     * @param int $announceId
+     * 
+     * @return array
+     */
+    public function getAnnounce($announceId)
+    {
+        $req = $this->db->prepare('SELECT announces.announce_id AS id, author_id AS authorId, users.username AS authorUsername, title, text, price, users.city AS city, announces.picture_id AS pictureId, pictures.name AS picturePath, announces.creation_date AS creationDate FROM announces LEFT JOIN users ON announces.author_id = users.user_id LEFT Join pictures ON announces.picture_id = pictures.picture_id WHERE announces.announce_id=?');
+
+        $announceData = ($req->execute([$announceId]))? $req->fetch() : false;
+
+        return $announceData;
+    }
+
+    public function getAnnouncesByCity($city)
+    {
+        $req = $this->db->prepare('SELECT announces.announce_id AS id, author_id AS authorId, users.username AS authorUsername, title, text, price, users.city AS city, announces.picture_id AS pictureId, pictures.name AS picturePath, announces.creation_date AS creationDate FROM announces LEFT JOIN users ON announces.author_id = users.user_id LEFT Join pictures ON announces.picture_id = pictures.picture_id WHERE users.city=?');
+
+        $announceData = ($req->execute([$city]))? $req->fetch() : false;
+
+        return $announceData;
+    }
+}
