249017e3bf8aeb6d256ff237549c6ec81817091e
mmallet66 <m.mallet@outlook.fr>
2020-04-20 12:07:12 +0200

    creation of conncetion view

---
+++ src/view/templates/connection.php
@@ -0,0 +1,23 @@
+<?php
+$this->title = 'Connexion';
+$this->scriptPath = '';
+?>
+<article class="content">
+    <h2>Se Connecter</h2>
+
+    <form action="#" method="post">
+        <p>
+            <label for="username">Nom d'utilisateur</label>
+            <input type="text" name="username" id="username">
+        </p>
+        <p>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password">
+        </p>
+        <p>
+            <input class="submit" type="submit" value="Me connecter">
+        </p>
+    </form>
+
+    <p>Vous n'êtes pas encore inscrit ? <a href="registration.html">S'inscrire</a></p>
+</article>
