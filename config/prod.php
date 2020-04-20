d904f08463683aca68074a3b0ecbc09d20523289
mmallet66 <m.mallet@outlook.fr>
2020-04-20 12:27:25 +0200

    define database connection constants

---
+++ config/prod.php
@@ -0,0 +1,8 @@
+<?php
+
+const SGBDR = 'mysql';
+const HOST = 'localhost';
+const DBNAME = 'occazou';
+const DSN = SGBDR.':host='.HOST.';dbname='.DBNAME.';charset=utf8';
+const USER = 'mathieu';
+const PASSWD = 'passe';
