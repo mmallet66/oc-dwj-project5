1cba9f1151e1001ab3eb9f2bfa9231acbcd38e01
mmallet66 <m.mallet66@outlook.fr>
2020-04-21 17:04:00 +0200

    creation of router method defineMethodName

--- src/Router.php
+++ src/Router.php
@@ -13,7 +13,7 @@ class Router
             if(isset($_GET['action'])):
                 if(in_array($_GET['action'], self::$views)):
                     $controller = new Controller\Controller();
-                    $method = 'get'.ucfirst($_GET['action']);
+                    $method = self::defineMethodName($_GET['action']);
 
                     (method_exists($controller,$method))? $controller->$method($_GET['req']) : $controller->getView($_GET['action']);
                 else:
@@ -30,5 +30,16 @@ class Router
             $controller = new Controller\Controller();
             $controller->getView('error', ['errorMessage'=>$errorMessage]);
         }
+    }
+
+    private static function defineMethodName(string $methodName)
+    {
+        $methodName = explode('-', $methodName);
+        foreach($methodName AS $key => $value):
+            $methodName[$key] = ($key == 0)? $value : ucfirst($value);
+        endforeach;
+        $methodName = implode('', $methodName);
+
+        return $methodName;
     }
 }
