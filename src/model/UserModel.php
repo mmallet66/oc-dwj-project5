764c32fac5d4a426aa3837cb770a6c5c5c331f00
mmallet66 <m.mallet66@outlook.fr>
2020-04-25 12:06:31 +0200

    update user model

--- src/model/UserModel.php
+++ src/model/UserModel.php
@@ -26,7 +26,7 @@ class UserModel extends Model
      */
     public function addUser(object $user)
     {
-        $req = $this->db->prepare('INSERT INTO users(username, password, gender, firstname, name, email, phone, address, city_name, zip_code) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
+        $req = $this->db->prepare('INSERT INTO users(username, password, gender, firstname, name, email, phone, address, city_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
 
         $affectedLine = $req->execute(array(
             $user->getUsername(),
@@ -37,8 +37,7 @@ class UserModel extends Model
             $user->getEmail(),
             $user->getPhone(),
             $user->getAddress(),
-            $user->getCityName(),
-            $user->getZipCode()
+            $user->city->getId()
         ));
         
         return $affectedLine;
@@ -61,7 +60,7 @@ class UserModel extends Model
      */
     public function getUser(string $username)
     {
-        $req = $this->db->prepare('SELECT user_id as id, username, password, creation_date as creationDate, gender, firstname, name, email, phone, address, city_name AS cityName, zip_code AS zipCode FROM users WHERE username=?');
+        $req = $this->db->prepare('SELECT users.user_id AS id, users.username, users.password, users.creation_date AS creationDate, users.gender, users.firstname, users.name, users.email, users.phone, users.address, cities.city_id AS city_id, cities.name AS city_name, cities.zip_code AS city_zipCode, cities.region_code AS city_regionCode, regions.name AS city_regionName from users INNER JOIN cities ON users.city_id = cities.city_id INNER JOIN regions ON cities.region_code = regions.code where username=?');
         $userData = ($req->execute(array($username)))? $req->fetch() : false;
 
         return $userData;
@@ -76,9 +75,9 @@ class UserModel extends Model
      */
     public function updateUser(object $user)
     {
-        $req = $this->db->prepare('UPDATE users SET username=:username, gender=:gender, firstname=:firstname, name=:name, email=:email, phone=:phone, address=:address, city_name=:cityName, zip_code=:ipCode WHERE user_id=:id');
+        $req = $this->db->prepare('UPDATE users SET username=:username, gender=:gender, firstname=:firstname, name=:name, email=:email, phone=:phone, address=:address, city_code=:cityCode WHERE user_id=:id');
 
-        return $req->execute(array(':username'=>$user->getUsername(), ':gender'=>$user->getGender(), ':firstname'=>$user->getFirstname(), ':name'=>$user->getName(), ':email'=>$user->getEmail(), ':phone'=>$user->getPhone(), ':address'=>$user->getAddress(), ':cityName'=>$user->getCityName(), ':zipCode'=>$user->getZipCode(), ':id'=>$user->getId()));
+        return $req->execute(array(':username'=>$user->getUsername(), ':gender'=>$user->getGender(), ':firstname'=>$user->getFirstname(), ':name'=>$user->getName(), ':email'=>$user->getEmail(), ':phone'=>$user->getPhone(), ':address'=>$user->getAddress(), ':cityCode'=>$user->getCityCode(), ':id'=>$user->getId()));
     }
 
     /**
