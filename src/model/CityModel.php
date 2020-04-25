5d895fe098f03a3bc4ef7caa8baefee67fdf74c7
mmallet66 <m.mallet66@outlook.fr>
2020-04-25 11:57:29 +0200

    creation of city object with its model

---
+++ src/model/CityModel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Occazou\Src\Model;
+
+require_once 'Model.php';
+
+/**
+ * Class UserDAO
+ */
+class CityModel extends Model
+{
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->dbConnect();
+    }
+
+    public function cityExists(string $zipCode, string $name)
+    {
+        $req = $this->db->prepare('SELECT city_id FROM cities WHERE zip_code=:zipCode && name=:name');
+        $req->execute([ ':zipCode'=>$zipCode, ':name'=>$name ]);
+        return $req->rowCount();
+    }
+
+    public function add(object $city)
+    {
+        $req = $this->db->prepare('INSERT INTO cities(name, zip_code, region_code) VALUES(?, ?, ?)');
+        return $req->execute([
+            $city->getName(),
+            $city->getZipCode(),
+            $city->getRegionCode()
+        ]);
+    }
+
+    public function getCity(string $zipCode, string $name)
+    {
+        $req = $this->db->prepare('SELECT city_id AS id, name, zip_code AS zipCode, region_code AS regionCode FROM cities WHERE zip_code=:zipCode && name=:name');
+        $req->execute([ ':zipCode'=>$zipCode, ':name'=>$name ]);
+        return $req->fetch();
+    }
+
+    public function getCityId(object $city)
+    {
+        if(!$this->cityExists($city->getZipCode(), $city->getName())):
+            $this->add($city);
+            $this->getCityId($city);
+        else:
+            return $this->getCity($city->getZipCode(), $city->getName())['id'];
+        endif;
+    }
+}
