5b2d4d5dc3528207c70ca6e7c4872c491d550295
mmallet66 <m.mallet66@outlook.fr>
2020-05-01 17:35:20 +0200

    add route in the router

--- src/Router.php
+++ src/Router.php
@@ -4,7 +4,7 @@ namespace Occazou\Src;
 
 class Router
 {
-    private static $views = ['connection', 'connect-user', 'search', 'disconnect-user', 'registration', 'new-user', 'create-announce', 'new-announce', 'user-account', 'update-user', 'update-password', 'user-announces'];
+    private static $views = ['connection', 'connect-user', 'search', 'disconnect-user', 'registration', 'new-user', 'create-announce', 'new-announce', 'user-account', 'update-user', 'update-password', 'user-announces', 'delete-announce'];
 
     public static function routeRequest()
     {
