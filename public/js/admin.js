3f0f15da3d2ba2b795b031aee2fa6825716b1e3a
mmallet66 <m.mallet66@outlook.fr>
2020-05-10 18:58:56 +0200

    creation of admin.js

---
+++ public/js/admin.js
@@ -0,0 +1,30 @@
+function listenClickOnButtons(arrayButtons) {
+    arrayButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const confirmed = requestConfirmation(this);
+            if(confirmed) {
+                document.location.href = '/admin/'+this.name+'/'+this.value;
+            }
+        })
+    });    
+}
+
+function requestConfirmation(button) {
+    let message = 'Êtes vous sur de vouloir ';
+    switch (button.name) {
+        case 'delete-user':
+            message += 'supprimer l\'utilisateur "'+button.value+'"';
+            break;
+    }
+    return confirm(message);
+}
+
+/* 
+##############################
+###          MAIN          ###
+##############################
+*/
+
+const buttons = document.querySelectorAll('button');
+
+listenClickOnButtons(buttons);
